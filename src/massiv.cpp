47f4e8e56cad6a30ecd897cb9b8553c141af79c3
unknown <naunadya>
2025-01-10 19:52:06 +0300

    menu

--- src/massiv.cpp
+++ src/massiv.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 #define SIZE 3
-void printMenu (const string arg [size])
+//void printMenu (const string arg [size])
     
 
     int main() {
@@ -10,23 +10,48 @@ void printMenu (const string arg [size])
     const string play_ [] = {"_Play_", "Settings", "Exit"};
     const string settings_ [] = {"Play", "_Settings_", "Exit"};
     const string exit_ [] = {"Play", "Settings", "_Exit_"};
-    int current = 0;
-    char key;
+    int index = 1;    
+    char key;    
 
-    while (true){
-        if (current ==0){
-            printMenu (play_);
-        } else if (current ==1){
-            printMenu (settings_);
-        } else {          
-            printMenu (exit_);
-        } 
-    }   
+    do {     
+        if (index ==1){
+            for (int i = 0; i < SIZE; i++){
+                cout << play_[i]  << endl;
+            }
+            
+        } else if (index ==2){
+            for (int i = 0; i < SIZE; i++){
+                cout << settings_[i]  << endl;
+            }
+            
+        } else  if (index ==3){          
+            for (int i = 0; i < SIZE; i++){
+                cout << exit_[i]  << endl;
+            } 
+        }  
+         
+    cout << "Enter key: ";
+    cin >> key;    
+    if (key =='w'){
+        if (index ==1){
+            index =3;
+        }
+        else {index -=1;
+        }
+        
+    } else if (key =='s'){
+        if (index == 3){
+            index =1;
+        }
+        else {index +=1;     
+        }
+    } 
+    } while (true);
     return 0;
 }
 
-void printMenu (const string arg [size]){
-     for (int i = 0; i < size; i++){                
-                cout <<  arg [i] << endl;
-            }     
-}
+//void printMenu (const string arg [size]){
+//     for (int i = 0; i < size; i++){                
+///                cout <<  arg [i] << endl;
+ //           }     
+//}
