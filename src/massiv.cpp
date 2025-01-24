5123f7d30cbb04030b880126ae0e460d17f7947a
unknown <naunadya>
2025-01-24 20:09:04 +0300

    need to end

--- src/massiv.cpp
+++ src/massiv.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 #define SIZE 3
 void printMenu (const string arg[SIZE]);  
-void printPlay (char operation, char key, int num1, int num2, int rez); 
-void printSettings (char operation, char key); 
-int printKey (int index);
+void printPlay (char operation, int index, char key); 
+char printSettings (int index, char key, char operation); 
+int printKey (int index, char operation_play, char operation, char key);
+    char operation_play;
 
     int main() {   
     const string play_ [] = {"_Play_", "Settings", "Exit"};
     const string settings_ [] = {"Play", "_Settings_", "Exit"};
     const string exit_ [] = {"Play", "Settings", "_Exit_"};
     int index = 1;
-    char key;
-    char operation = '+';           
+    char key;    
+    char operation;           
     while (true) {     
         if (index ==1){
             printMenu (play_) ;
@@ -24,10 +25,17 @@ int printKey (int index);
            //     printSettings;           
             } else  if (index ==3){     
                 printMenu (exit_);           
-                }            
+            }            
         
-    index = printKey (index);  
+    index = printKey ( index,  operation_play,  operation,  key);  
     } 
+    // if (key == 'e' && index ==1){
+    //    printPlay (operation, index);
+    //} else 
+   //if (key == 'e' && index == 2){
+     //   //cout << "123"<< endl;
+       // printSettings ( index) ;  
+    //}
     return 0;
 }
 
@@ -40,8 +48,9 @@ void printMenu(const string arg[SIZE]){
  //   cin >> key;   
 }
 
-int printKey (int index){
-    char key;
+int printKey (int index, char operation_play, char operation, char key){
+   // char key;
+    cout << operation_play;
     cout << "Enter key: ";
     cin >> key; 
        
@@ -58,61 +67,62 @@ int printKey (int index){
         else {index +=1;     
         }
     } else if (key == 'e' && index ==1){
-        printPlay (char operation, char key, int num1, int num2, int rez);
-    } else if (key == 'e' && index == 2){
+        printPlay (operation_play, index, key);
+        operation_play = operation;
+    }else if (key == 'e' && index == 2){
         //cout << "123"<< endl;
-        printSettings (char operation, char key) ;     
-        
+        printSettings ( index, key, operation_play) ;  
     }
     return index;
 } 
 
-void printPlay(char operation, char key, int num1, int num2, int rez){
-    //int index;
+void printPlay( char operation_play, int index, char key){
     //char key;  
     //cout << "Enter key: ";    
     
-    //if (key =='e')
-    //&& index ==1)
+   // if (key =='e' && index ==1)
         while (key != 'q'){
-            //int num1, num2, rez;
+             cout << operation_play;
+            int num1, num2, rez;
             cout << "Enter number 1: ";
             cin >> num1;
             cout << "Enter number 2: ";
             cin >> num2;
-                if ( operation =='+'){
-                rez = num1 + num2;
-                }else if (operation =='-'){
+          // cout << operation;
+                if ( operation_play =='+'){
+                    rez = num1 + num2;
+                }else if (operation_play =='-'){
                     rez = num1 - num2;
-                } else if (operation =='*'){
+                } else if (operation_play =='*'){
                     rez = num1 * num2;
-                }else if (operation =='/'&& num2 !=0){
+                }else if (operation_play =='/'&& num2 !=0){
                     rez = num1 / num2;
                 }else {
-                 if (operation =='/'&& num2 !=0){
+                 if (operation_play =='/'&& num2 !=0){
                     cout << "Error"<< endl;
                     break;
-                }             
+                } 
+                }           
             cout << "Rezult = " << rez<< endl;
             cout << "Enter key: ";
             cin >> key;
-            }
+            
     }    
 }
 
-void printSettings(char operation, char key){
-    //int index;
-    //char key;
+char printSettings(int index, char key, char operation_play){
+    char operation;
     //cout << "Enter key: ";
     //cin >> key;
     //char operation;
-        //if (index ==2 )
-        //&& key == 'e')               
+      //  if (index ==2&& key == 'e')               
         while (key != 'q'){
             cout << "Enter operation: ";
             cin >> operation;
             cout << "Enter key: ";
             cin >> key;     
-            }        
+            } 
+            cout << operation;
+            return operation;       
     } 
-//}
+
